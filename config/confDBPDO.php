fb0b14625072c902e7ebac8187dd87e04dfb0fe9
Alex <alex.asesan@educa.jcyl.es>
2024-11-22 13:26:10 +0100

    Arreglo fichero config

--- config/confDBPDO.php
+++ config/confDBPDO.php
@@ -2,7 +2,7 @@
 
     // CLASE
 
-    
+    /*
     //Ip de la maquina donde esta alojada, puerto que se usara y nombre de la base de datos a conectarse
     define('CONEXION', 'mysql:host=daw204.isauces.local;port=3306;dbname=DB204DWESProyectoTema4');
     
@@ -11,7 +11,7 @@
     
     //Contraseña del usuario que usaremos para conectarnos
     define('CONTRASEÑA', 'paso');
-    
+    */
     
     //CASA
     /*
